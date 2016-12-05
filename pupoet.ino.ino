7bb568b1e0a8fdee52dfe8a9b8f942bd48caac68
puputnovaliah <puputnovaliah@gmail.com>
2016-12-05 17:18:35 +0700

    Add files via upload

---
+++ pupoet.ino.ino
@@ -0,0 +1,20 @@
+#include<SoftwareSerial.h>
+SoftwareSerial mySerial(9, 10);
+int sensor=7; //The output of PIR sensor connected to pin 7
+int push_switch=6; // push button switch connected to pin 6
+int buzzer=8; // buzzer connected at pin 8
+int sensor_value; //variable to hold read sensor value
+int sms_count=0;
+void setup() {
+  pinMode(sensor,INPUT); // configuring pin 7 as Input
+  pinMode(push_switch,INPUT); // configuring pin 6 as Input
+  pinMode(buzzer,OUTPUT); // configuring pin 8 as OUTPUT
+mySerial.begin(9600);
+
+}
+
+void loop() {
+Check_Burglar();// subroutine to check sensor status and activation of outputs
+Check_Reset(); // subroutine to check if alarm reset switch pressed or not
+
+}
